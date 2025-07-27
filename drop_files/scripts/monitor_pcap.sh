f6db3600245b73623bea6b2b637b912ef892485e
dleto614 <dlebeau.03@protonmail.com>
2025-07-27 19:54:32 -0400

    I think now this is all working properly. Will come back to it later, but wrote a seperate script to run bruteshark to extract Network Diagram, DNS, Credentials, but also wrote a different script to extract files using TCPFlow + Foremost which I like a lot better. Worked out a few bugs, but I suspect that wait isn't actually waiting properly. I just have been working on this far too long and don't want to really debug this right now.

--- drop_files/scripts/monitor_pcap.sh
+++ drop_files/scripts/monitor_pcap.sh
@@ -19,7 +19,7 @@ do
     echo "No files to process. Cleaning up empty directories and files than Sleeping..."
 
     find "$WATCH_DIR" -mindepth 1 -depth -type d -empty -delete
-    find "/opt/drop_files/results" -mindepth 1 -depth -type d -empty ! -name "bruteshark" -delete
+    find "/opt/drop_files/results" -mindepth 1 -depth -type d -empty ! -name "/opt/drop_files/results/db/bruteshark*" -delete
 
     sleep 10
     continue
@@ -28,7 +28,7 @@ do
   while IFS= read -r file
   do
 
-    echo "$(date '+%Y-%m-%d %H:%M:%S') Starting processing: '$file'" >> /opt/drop_files/scripts/logs/monitor_files.log
+    echo "[$(date)] Starting processing: '$file'" >> /opt/drop_files/scripts/logs/monitor_files.log
 
     target="$PROCESSED_DIR/$(basename "$file")"
     if [[ -e "$target" ]]; then
@@ -39,16 +39,21 @@ do
     mv "$file" "$target"
 
     /bin/bash /opt/drop_files/scripts/parse_pcap.sh "$target" &
+    /bin/bash /opt/drop_files/scripts/extract_files.sh "$target" &
+    /bin/bash /opt/drop_files/scripts/run_bruteshark.sh "$target" &
 
-	((count++))
+    ((count++))
 
-	if [ $((count % MAX_JOBS)) -eq 0 ]
+    if [ $((count % MAX_JOBS)) -eq 0 ]
     then
-        echo "Reached maximum of number of processes to run in the background"
-        echo "Waiting for them to finish in the background"
-        wait # Wait for the current background processes to finish running before continuing
+          echo "Reached maximum of number of processes to run in the background"
+          echo "Waiting for them to finish in the background"
+          wait # Wait for the current background processes to finish running before continuing
     fi
   done <<< "$files"
+
+  echo "[$(date)] Finished processing pcaps. Waiting for last background processes to finish." >> /opt/drop_files/scripts/logs/monitor_files.log
+  wait
 
   find "$WATCH_DIR" -mindepth 1 -depth -type d -empty -delete
 
--- drop_files/scripts/parse_pcap.sh
+++ drop_files/scripts/parse_pcap.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-today=$(date +%m%d%Y)
-results_uuid=$(uuidgen)
-
-results_folder=/opt/drop_files/results/$today/$results_uuid
-json_folder=/opt/drop_files/results/json
-
+# Take input of the PCAP file to process
 FILE="$1"
 
+results_folder_uuid=$(date +%m%d%Y)
+results_pcap_uuid=$(uuidgen)
+
+results_folder=/opt/drop_files/results/$results_folder_uuid/$results_pcap_uuid
+json_folder=/opt/drop_files/results/json/$results_folder_uuid/$results_pcap_uuid
+
+# This will be the final output
+db=/opt/drop_files/results/db/pcap_analysis/$results_folder_uuid/$results_pcap_uuid
+
+mkdir -p $db
+
 # Yes I hate it very much
-mkdir -p $json_folder/zeek/$results_uuid
+mkdir -p $json_folder/zeek
 mkdir -p $json_folder/dsniff
 mkdir -p $json_folder/p0f
 mkdir -p $json_folder/ntlmraw_unhide
@@ -18,62 +24,65 @@ mkdir -p $json_folder/ntlmraw_unhide
 # Yes. I hate it.
 mkdir -p $results_folder/dsniff
 mkdir -p $results_folder/p0f
-mkdir -p $results_folder/bruteshark
 mkdir -p $results_folder/ntlmraw_unhide
 
 current=$(date)
 echo "[$current] Processing file "$FILE"" >> /opt/drop_files/scripts/logs/processed_pcaps.log
 
-if [[ -d "$json_folder/zeek/$results_uuid" && -w "$json_folder/zeek/$results_uuid" ]]
+if [[ -d "$json_folder/zeek" && -w "$json_folder/zeek" ]]
 then
-	cd $json_folder/zeek/$results_uuid
+	cd $json_folder/zeek
   	/opt/zeek/bin/zeek -C \
     	LogAscii::use_json=T \
     	-r "$FILE"
 	cd $results_folder
 else
-  echo "Error: Cannot write to $json_folder/zeek/$results_uuid" >> /opt/drop_files/scripts/logs/processed_pcaps.log
+  echo "Error: Cannot write to $json_folder/zeek" >> /opt/drop_files/scripts/logs/processed_pcaps.log
 fi
 
-dsniff -p "$FILE" -w $results_folder/dsniff/dsniff_results.log > /dev/null 2>&1 &
-p0f -r "$FILE" -o $results_folder/p0f/p0f.log > /dev/null  2>&1 &
+dsniff -p "$FILE" -w $results_folder/dsniff/dsniff_results.log > /dev/null &
+p0f -r "$FILE" -o $results_folder/p0f/p0f.log > /dev/null &
 
-python3 /opt/NTLMRawUnHide.py -q -i "$FILE" -o $results_folder/ntlmraw_unhide/ntlmraw_unhide.log > /dev/null 2>&1 &
+python3 /opt/NTLMRawUnHide.py -q -i "$FILE" -o $results_folder/ntlmraw_unhide/ntlmraw_unhide.log > /dev/null &
 
 # Wait for all the background processes to be done
 wait
 
+echo "[$(date)] Finished running tools on file "$FILE"" >> /opt/drop_files/scripts/logs/processed_pcaps.log
+
+# Start checking for logs
 check_p0f=$(find $results_folder -type f -name "p0f.log")
 check_dsniff=$(find $results_folder -type f -name "dsniff_results.log")
 check_ntlmrawunhide=$(find $results_folder -type f -name "ntlmraw_unhide.log")
 
+# If the logs are not empty
 if [[ -n "$check_p0f" && -s "$check_p0f" ]]
 then
-	jq -Rn '[.,inputs] | map({p0f: .})' "$check_p0f" > $json_folder/p0f/$results_uuid.json
+	jq -Rn '[.,inputs] | map({p0f: .})' "$check_p0f" > $json_folder/p0f/$results_pcap_uuid.json
 else
-	echo "No p0f output for $FILE" >> /opt/drop_files/scripts/logs/processed_pcaps.log
+	echo "[$(date)] No p0f output for $FILE" >> /opt/drop_files/scripts/logs/processed_pcaps.log
 	rm -rf $results_folder/p0f
 fi
 
 if [[ -n "$check_dsniff" && -s "$check_dsniff" ]]
 then
-	jq -Rn '[.,inputs] | map({dsniff: .})' "$check_dsniff" > $json_folder/dsniff/$results_uuid.json
+	jq -Rn '[.,inputs] | map({dsniff: .})' "$check_dsniff" > $json_folder/dsniff/$results_pcap_uuid.json
 else
-	echo "No dsniff output for $FILE" >> /opt/drop_files/scripts/logs/processed_pcaps.log
+	echo "[$(date)] No dsniff output for $FILE" >> /opt/drop_files/scripts/logs/processed_pcaps.log
 	rm -rf $results_folder/dsniff
 fi
 
 if [[ -n "$check_ntlmrawunhide" && -s "$check_ntlmrawunhide" ]]
 then
-	jq -Rn '[.,inputs] | map({ntlm: .})' "$check_ntlmrawunhide" > $json_folder/ntlmraw_unhide/$results_uuid.json
+	jq -Rn '[.,inputs] | map({ntlm: .})' "$check_ntlmrawunhide" > $json_folder/ntlmraw_unhide/$results_pcap_uuid.json
 else
-	echo "No ntlmraw_unhide output for $FILE" >> /opt/drop_files/scripts/logs/processed_pcaps.log
+	echo "[$(date)] No ntlmraw_unhide output for $FILE" >> /opt/drop_files/scripts/logs/processed_pcaps.log
 	rm -rf $results_folder/ntlmraw_unhide
 fi
 
-brutesharkcli -i "$FILE" -m Credentials,NetworkMap,DNS,FileExtracting -o $results_folder/bruteshark > /dev/null &
-
-/opt/drop_files/scripts/merge_json.sh "$results_uuid"
+# Merge json files to get final output.
+/opt/drop_files/scripts/merge_json.sh "$json_folder" "$db" "$results_pcap_uuid"
 
 finished=$(date)
 echo "[$finished] Processed file "$FILE"" >> /opt/drop_files/scripts/logs/processed_pcaps.log
+
